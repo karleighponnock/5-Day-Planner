7fbe0d520a6fdff23197753b4320e11435e1650b
karleighponnock <karleighponnock@gmail.com>
2020-06-18 18:32:35 -0400

    saving to local storagegit add -Agit add -A

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+var tasks = localStorage.getItem("tasks")
+if (tasks) {
+    tasks = JSON.parse(tasks)
+} else {
+    tasks = {}   // hour: info
+}
+console.log(tasks)
+
+function showLocalStorage() {
+
+    for (key in tasks) {
+console.log("--->", key, tasks[key])
+        $("#" + key).siblings(".description").children().val(tasks[key])
+
+    }
+}
+
+function updatecolors() {
 
 
+}
+function init() {
+    showLocalStorage()
+    updatecolors()
+}
 //log and track date and time
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
@@ -7,8 +30,21 @@ console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 // Display current day and date in p tag 
 //update every second so when the date switches so does disply
 setInterval(() => {
-    $("p#currentDay").text(moment().format("dddd, MMM Do YY"));  
+    $("p#currentDay").text(moment().format("dddd, MMM Do YY"));
 }, 1000);
+
+
+$(".saveBtn").on("click", function () {
+    console.log(this)
+    // get the input value from the textarear
+    var input = $(this).siblings(".description").children().val()
+    var hour = $(this).attr("id")
+    tasks[hour] = input
+    console.log(tasks)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+
+})
+//
 
 
 
@@ -22,3 +58,6 @@ setInterval(() => {
 // //if hour is after moment
 // $(".row").addClass("future");
 // }
+
+
+init()
