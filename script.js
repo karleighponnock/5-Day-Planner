026bc86689dfbe4086c38d97ee86b000ff11daee
karleighponnock <karleighponnock@gmail.com>
2020-06-18 16:55:56 -0400

    date is displayed to be updated as it changes and timer is being tracked in console

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 
+
 //log and track date and time
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+// Display current day and date in p tag 
+//update every second so when the date switches so does disply
+setInterval(() => {
+    $("p#currentDay").text(moment().format("dddd, MMM Do YY"));  
+}, 1000);
+
+
+
+//if hour is before moment
+// if(){
+// $(".row").addClass("past");
+// } if()
+// //if hour the same as moment
+// $(".row").addClass("present");
+// } else
+// //if hour is after moment
+// $(".row").addClass("future");
+// }
